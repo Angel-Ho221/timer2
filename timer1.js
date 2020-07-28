959c33ed627e20bab8e557ef76b6c5f2cdafe4da
Angel Ho <66530174+Angel-Ho221@users.noreply.github.com>
2020-07-28 14:44:06 -0400

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -1,12 +1,12 @@
 let time = process.argv.slice(2);
 console.log('myArgs: ', time);
 
-// const time = [ '10', '3', '5', '15', '9' ];
+
 const timer = (time) => {
 
   for (let element of time) { // gives me the element
-    if (!isNaN(element) && element >= 0) { //only if it is a number and is or greater than 0
-      setTimeout(() => process.stdout.write("\x07"), element * 1000)// *1000 to get ms
+    if (!isNaN(element) && element >= 0) { than 0
+      setTimeout(() => process.stdout.write("\x07"), element * 1000)// 
     }
   }
 };
